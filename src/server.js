cbbf2ae9f1bd9042c85be080d312e980c1e09af4
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-25 14:16:11 +0000

    file running

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
-import express from 'express'
+import express from "express" 
+import listEndpoints from "express-list-endpoints"
+
+import usersRouter from "./services/users/index.js"
 
 const server = express()
-const port = 3001
 
-server.listen(3001,()=>{
-    console.log("server is running on port 3001")
-} )
+const port = 3001 
+
+server.use(express.json())
+
+
+server.use("/users", usersRouter)
+
+
+console.table(listEndpoints(server))
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
